9a479bc1a99e55751b507b9443281458a1cc76d2
Adhyan-tri <adhyanjaiswal86.aj@gmail.com>
2022-04-07 11:09:38 +0530

    commit this project

---
+++ src/main/java/com/springBoot/SpringBoot1/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.springBoot.SpringBoot1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome to spring boot application";
+    }
+}
